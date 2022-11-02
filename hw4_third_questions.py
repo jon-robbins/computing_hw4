18c767ae8a529362d97a87bdc4aa58f5e8660c55
Jon Robbins <jon@ciutadella-128-125.eduroamcc.upf.edu>
2022-11-02 14:25:42 +0100

    hw4_first_questions.py

---
+++ hw4_third_questions.py
@@ -0,0 +1,60 @@
+
+
+
+
+##############
+
+# Now imagine you have a certain data structure that
+# contains information about different countries and
+# the number of people who was registered with covid
+# in a weekly basis.
+# e.g. {'Spain': [4, 8, 2, 0, 1], 'France': [2, 3, 6],
+#       'Italy': [6, 8, 1, 7]}
+# Assuming that the moment they started reporting the
+# number of registered cases is not the same (thus
+# the length of the lists can differ)
+
+
+# 7)
+# Create a function called "total_registered_cases"
+# that has 2 parameters:
+# 1) The data structure described above.
+# 2) A string with the country name.
+#
+# The function should return the total number of cases
+# registered so far in that country
+
+
+# 8)
+# Create a function called "total_registered_cases_per_country"
+# that has 1 parameter:
+# 1) The data structure described above.
+#
+# The function should return a dictionary with a key
+# per each country and as value the total number of cases
+# registered so far that the country had
+#
+
+
+# 9)
+# Create a function called "country_with_most_cases"
+# that has 1 parameter:
+# 1) The data structure described above
+#
+# The function should return the country with the
+# greatest total amount of cases
+
+
+
+###############
+# Use the data in covid.csv for this exercise
+#
+# 10) In a separate file, write a piece of code that
+# loads the covid.csv file and prints the list of countries
+#  and the total average of death/confirmed among those countries
+# for those countries that have more than 500, 1000 and 5000
+# active cases respectively.
+# Follow DRY principles in order to complete this exercise.
+#
+#
+# #
