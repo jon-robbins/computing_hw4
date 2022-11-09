fc60852c0831e6996774ff245967046cf93efb09
wangchunfeng <chunfeng.wang@bse.eu>
2022-11-09 17:22:59 +0100

    1109wcf

---
+++ hw4_third_questions.py
@@ -0,0 +1,43 @@
+# 7)
+# Create a function called "total_registered_cases"
+# that has 2 parameters:
+# 1) The data structure described above.
+# 2) A string with the country name.
+#
+# The function should return the total number of cases
+# registered so far in that country
+def total_registered_cases(raw_data,country_name):
+    for item in raw_data.items():
+        if item[0] == country_name:
+            return sum(item[1])
+
+# 8)
+# Create a function called "total_registered_cases_per_country"
+# that has 1 parameter:
+# 1) The data structure described above.
+#
+# The function should return a dictionary with a key
+# per each country and as value the total number of cases
+# registered so far that the country had
+#
+def total_registered_cases_per_country(raw_data):
+    map = {}
+    for item in raw_data.items():
+        map[item[0]] = sum(item[1])
+    return map
+
+# 9)
+# Create a function called "country_with_most_cases"
+# that has 1 parameter:
+# 1) The data structure described above
+#
+# The function should return the country with the
+# greatest total amount of cases
+def country_with_most_cases(raw_data):
+    country = ""
+    cases = 0
+    for item in raw_data.items():
+        if  sum(item[1]) > cases :
+            cases = sum(item[1])
+            country = item[0]
+    return country,cases
